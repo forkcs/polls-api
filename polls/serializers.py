3120488f19e923f218c1d69b5a2185a001ed6b11
Fedor Soldatkin <forkcs@yandex.ru>
2021-06-04 00:24:08 +0300

    Write serializers for Poll and Question
    QuestionSerializer, PollListSerializer, PollSerializer

---
+++ polls/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework.serializers import ModelSerializer
+
+from polls.models import Poll, Question, Answer, PollResult, Choice
+
+
+class QuestionSerializer(ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ('id', 'poll_id', 'text', 'answer_type')
+
+
+class PollListItemSerializer(ModelSerializer):
+    class Meta:
+        model = Poll
+        fields = ('id', 'title', 'description', 'start_date', 'end_date')
+
+
+class PollSerializer(ModelSerializer):
+    questions = QuestionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Poll
+        fields = ('id', 'title', 'description', 'start_date', 'end_date', 'questions')
