14e5669e9899c742a1546cef1404effe11f73f7e
Fedor Soldatkin <forkcs@yandex.ru>
2021-06-03 22:21:52 +0300

    Write ORM models for polls app
    Add Poll, Question, Choice, PollResult and Answer.

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,45 @@
 from django.db import models
 
 
-# todo
+class Poll(models.Model):
+    title = models.CharField(max_length=1000, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+ANSWER_TYPES = (
+    ('text', 'Plain text'),
+    ('single_choice', 'Single choice'),
+    ('mul_choices', 'Multiple choices')
+)
+
+
+class Question(models.Model):
+    poll = models.ForeignKey(to='Poll', blank=False, null=False, on_delete=models.CASCADE)
+
+    text = models.TextField(blank=False, null=False)
+    answer_type = models.CharField(max_length=100, blank=True, null=False, default='text', choices=ANSWER_TYPES)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(to='Question', blank=False, null=False, on_delete=models.CASCADE)
+
+    text = models.CharField(max_length=1000, blank=False, null=False)
+
+
+class PollResult(models.Model):
+    poll = models.ForeignKey(to='Poll', blank=False, null=True, on_delete=models.SET_NULL)
+    user_uid = models.IntegerField(blank=False, null=False)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(to='Question', blank=False, null=False, on_delete=models.CASCADE)
+    poll_result = models.ForeignKey(to='PollResult', blank=False, null=False, on_delete=models.CASCADE)
+
+    text = models.TextField(blank=True, null=True)
+    choices = models.ManyToManyField(to='Choice')
