bf9ab52df3f9a0a99c96bc9fad5c5c7d105d9ab8
Fedor Soldatkin <forkcs@yandex.ru>
2021-06-04 00:24:49 +0300

    Write two views for CRUD polls

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from polls.serializers import PollListItemSerializer, PollSerializer
+from polls.models import Poll
 
 
-# todo
+class PollsListView(ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollListItemSerializer
+
+
+class PollsDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
