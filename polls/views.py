b325dffddb527b32fb32283c16567799924e3870
Fedor Soldatkin <forkcs@yandex.ru>
2021-06-04 17:02:08 +0300

    Configure permissions for PollsDetailView

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from polls.serializers import PollListItemSerializer, PollSerializer
 from polls.models import Poll
@@ -12,3 +13,4 @@ class PollsListView(ListCreateAPIView):
 class PollsDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
