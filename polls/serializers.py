5be224d520221ce693751521ebe5394565f1d2bd
Fedor Soldatkin <forkcs@yandex.ru>
2021-06-04 17:02:37 +0300

    Make start_date field in poll serializer read-only

--- polls/serializers.py
+++ polls/serializers.py
@@ -13,6 +13,7 @@ class PollListItemSerializer(ModelSerializer):
     class Meta:
         model = Poll
         fields = ('id', 'title', 'description', 'start_date', 'end_date')
+        read_only_fields = ('start_date',)
 
 
 class PollSerializer(ModelSerializer):
