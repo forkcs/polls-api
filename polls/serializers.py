b6848065faaa8978f2e8c46699098d859b59ae43
Fedor Soldatkin <forkcs@yandex.ru>
2021-06-05 14:31:25 +0300

    Specify read-only fields in existing serializers

--- polls/serializers.py
+++ polls/serializers.py
@@ -3,22 +3,33 @@ from rest_framework.serializers import ModelSerializer
 from polls.models import Poll, Question, Answer, PollResult, Choice
 
 
+class ChoiceSerializer(ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = ('id', 'question_id', 'text')
+        read_only_fields = ('id',)
+
+
 class QuestionSerializer(ModelSerializer):
+    choices = ChoiceSerializer(many=True, read_only=False)
+
     class Meta:
         model = Question
-        fields = ('id', 'poll_id', 'text', 'answer_type')
+        fields = ('id', 'poll_id', 'text', 'answer_type', 'choices')
+        read_only_fields = ('id',)
 
 
 class PollListItemSerializer(ModelSerializer):
     class Meta:
         model = Poll
         fields = ('id', 'title', 'description', 'start_date', 'end_date')
-        read_only_fields = ('start_date',)
+        read_only_fields = ('start_date', 'id')
 
 
 class PollSerializer(ModelSerializer):
-    questions = QuestionSerializer(many=True, read_only=True)
+    questions = QuestionSerializer(many=True, read_only=False)
 
     class Meta:
         model = Poll
         fields = ('id', 'title', 'description', 'start_date', 'end_date', 'questions')
+        read_only_fields = ('start_date', 'id')
